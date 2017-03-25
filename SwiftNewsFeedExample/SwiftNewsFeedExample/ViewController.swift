e4a47a6aca3f67df2a590fb41d8ce5ab3bea3d1b
haiderashfaq <donpy07@hotmail.co.uk>
2017-03-25 13:55:46 +0000

    Created xcode project
    + Created initial xcode project and codebase

---
+++ SwiftNewsFeedExample/SwiftNewsFeedExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftNewsFeedExample
+//
+//  Created by Haider Ashfaq on 25/03/2017.
+//  Copyright © 2017 Haider Ashfaq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
